fc0ae19905a31543945f19af72368e0375c19ec0
anvaka <anvaka@gmail.com>
2014-06-16 21:53:42 -0700

    Watch all files in src for browserify

--- app/templates/_gulpfile.js
+++ app/templates/_gulpfile.js
@@ -14,7 +14,7 @@ var devServer = {
 };
 
 var paths = {
-  scripts: ['src/**/*.js'],
+  scripts: ['src/**/*.*'],
   markup: ['src/*.html'],
   styles: { paths: [ path.join(__dirname, 'src/styles') ] }
 };
